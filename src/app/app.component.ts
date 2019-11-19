8b41d682832d7f830c0f0c7a9de3d9ed9e3c243c
aleami <alessia.amitranobo@gmail.com>
2019-11-19 17:17:37 +0000

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngReusableBtn';
+  message = 'Click on a button';
+  textBtnConfig = {
+    styles: {
+      position: 'relative',
+      width: '150px',
+      height: '60px',
+      backgroundColor: '#000',
+      color: '#fff',
+      fontFamily: 'sans-serif',
+      fontSize: '20px',
+      borderRadius: '10px'
+    },
+    text: 'Click Here'
+  };
+
+  imgBtnConfig = {
+    styles: {
+      position: 'relative',
+      width: '50px',
+      height: '50px'
+    },
+    src: './assets/user-shape.png'
+  };
+
+  onClickEventReceived(event: string) {
+    this.message = event;
+  }
 }
--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,7 +7,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() buttonConfig: any;
+  @Output() textBtnClickEmt: EventEmitter<string> = new EventEmitter<string>();
+  @Output() imgBtnClickEmt: EventEmitter<string> = new EventEmitter<string>();
   constructor() {}
 
   ngOnInit() {}
+
+  onTextBtnClick() {
+    this.textBtnClickEmt.emit('You have clicked on a text button.');
+  }
+
+  onImgBtnClick() {
+    this.imgBtnClickEmt.emit('You have clicked on an image button.');
+  }
 }
