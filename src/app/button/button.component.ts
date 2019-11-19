70cad05a6c119f80e143b3948bd5aedc01fbec1c
aleami <alessia.amitranobo@gmail.com>
2019-11-19 16:15:39 +0000

    [wip] first structure

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+  @Input() buttonConfig: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
